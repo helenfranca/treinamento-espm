3ec74a8797092e29cbfa0e3d3ca4d7408c102eef
Marcos A C de Paula <cardepaula@gmail.com>
2018-08-15 18:50:40 -0300

    lista-detalhes

--- src/pages/lista-concursos/lista-concursos.ts
+++ src/pages/lista-concursos/lista-concursos.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+
+import { DetalhesConcursoPage } from "./../detalhes-concurso/detalhes-concurso";
 
 /**
  * Generated class for the ListaConcursosPage page.
@@ -10,16 +12,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-lista-concursos',
-  templateUrl: 'lista-concursos.html',
+  selector: "page-lista-concursos",
+  templateUrl: "lista-concursos.html"
 })
 export class ListaConcursosPage {
+  concursos: Array<{ orgao: string; edital: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.concursos = [
+      { orgao: "SEDU", edital: "09/2015" },
+      { orgao: "PRODEST", edital: "01/2018" }
+    ];
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ListaConcursosPage');
+    console.log("ionViewDidLoad ListaConcursosPage");
   }
 
+  verDetalhes(event, conc) {
+    this.navCtrl.push(DetalhesConcursoPage, { conc: conc });
+  }
 }
