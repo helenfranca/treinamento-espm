df3083c8e27885b87850c106cad62baa435903fa
Marcos A C de Paula <cardepaula@gmail.com>
2018-08-16 13:26:08 -0300

    usando um novo branch

--- src/pages/lista-concursos/lista-concursos.ts
+++ src/pages/lista-concursos/lista-concursos.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
 /**
  * Generated class for the ListaConcursosPage page.
@@ -10,16 +10,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-lista-concursos',
-  templateUrl: 'lista-concursos.html',
+  selector: "page-lista-concursos",
+  templateUrl: "lista-concursos.html"
 })
 export class ListaConcursosPage {
+  candidato: any;
+  concursos: Array<{ orgao: string; edital: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //this.candidato = navParams.get("candidato");
+    this.concursos = [
+      { orgao: "SEDU", edital: "09/2015" },
+      { orgao: "PRODEST", edital: "01/2018" }
+    ];
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ListaConcursosPage');
+    console.log("ionViewDidLoad ListaConcursosPage");
   }
 
+  verDetalhes(event, conc) {
+    //this.navCtrl.push(DetalhesConcursoPage, { conc: conc });
+  }
+
+  buscaConcursos(event, candidato) {}
 }
